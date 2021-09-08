66af661ea36f5ed2693a3ad30409e091ef6386ef
DESKTOP-N4SLG50\dania <ererer>
2021-09-08 12:33:40 +0400

    init

---
+++ КАЛкулятор/Program1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace КАЛкулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i > 0)
+            {
+
+            double operant1;
+            Console.WriteLine("Введите первое число!");
+            operant1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ведите второе число!");
+            double operant2;
+            operant2 = double.Parse(Console.ReadLine());
+           
+
+            string operation;
+            Console.WriteLine("Выберите арифм действие "  +
+                "\nНапример + или - или * или /");
+            operation = Console.ReadLine();
+
+                double answer;
+
+                switch (operation)
+                {
+                    case "+":
+                        answer = operant1 + operant2;                       
+                        break;
+                    case "-":
+                        answer = operant1 - operant2;
+                        break;
+                    case "*":
+                       answer = operant1 * operant2;
+                        break;
+                    case "/":
+                        answer = operant1 / operant2;
+                        break;
+                    default:
+                        Console.WriteLine("Неправильно выбрано действие");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Массивы/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Массивы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
